ea85b23e0bd583a62e9de899f72ddec864402a1f
Elisey Timoshenko <timoshenko_em@mmz.group>
2026-04-29 17:43:23 +0700

    fix: из main убрано лишнее

--- src/main.c
+++ src/main.c
@@ -1,22 +1,11 @@
 #include "app/app.h"
 
-#include <locale.h>
-
-#ifdef _WIN32
-#include <windows.h>
-#endif
-
-int main(void) {
-#ifdef _WIN32
-    SetConsoleCP(CP_UTF8);
-    SetConsoleOutputCP(CP_UTF8);
-#endif
-
-    setlocale(LC_ALL, "");
-
+int main(void) 
+{
     int status = 0;
     app_t app;
-    if (app_init(&app) == 0) {
+    if (app_init(&app) == 0) 
+    {
         status =  app_run(&app);
     }
    
